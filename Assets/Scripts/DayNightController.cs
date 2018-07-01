517006ce64abfb7db8799b20109bfa787d3573db
bai <baihuirong111@126.com>
2018-07-01 16:55:16 +0800

    wrote a readme file

---
+++ Assets/Scripts/DayNightController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightController : MonoBehaviour {
+
+	public float rotateSpeed = 10;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.up *rotateSpeed*Time.deltaTime,Space.Self);
+		
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+	public GameObject BodyInventory;
+
+	// Use this for initialization
+	void Start () {
+		
+	} 
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Tab))
+		{
+			if (BodyInventory.activeInHierarchy == true) {
+				BodyInventory.SetActive (false);
+			} else 
+			{ 
+				BodyInventory.SetActive (true);
+			}
+
+		}
+	}
+}
